8e312d76c6494f864e5811e0a6e3fa0e7eef49f3
Ajay Ghosh M <ajayghosh668@gmail.com>
2022-01-04 10:19:43 +0530

    Added instance variables in Tasks controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_063314) do
+ActiveRecord::Schema.define(version: 2022_01_03_062318) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "title"
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
